975ae3f969987677cc7d0dfeefb7b1f1469252d3
Leonid <leokorrr@gmail.com>
2022-12-02 17:01:02 +0100

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProjectsModule } from './projects/projects.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { ContactInfoModule } from './contact-info/contact-info.module';
+import { SocialLinksModule } from './social-links/social-links.module';
+
+@Module({
+  imports: [
+    ProjectsModule,
+    ReviewsModule,
+    ContactInfoModule,
+    SocialLinksModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
